4c072ba6b8da5d075dfdddbbec5406f623b0d5e7
tundeakins <babatundeakinsanmi@gmail.com>
2023-10-31 14:06:35 +0100

    fix test again

--- tests/test_radius_conv.py
+++ tests/test_radius_conv.py
@@ -3,21 +3,21 @@ import numpy as np
 
 def test_convert_radius():
     # Test conversion from spherical to deformed radius
-    R_pl = 1.0
-    hf = 0.1
-    qmass = 0.01
+    Rp = 0.1
+    hf = 1.5
+    qmass = 0.00098
     inc = 90.0
-    aR = 10.0
+    aR = 3.0
     conv = "Rp2Rv"
-    R_fit = convert_radius(R_pl, hf, qmass, inc, aR, conv=conv)
-    assert np.isclose(R_fit, 1.011, rtol=1e-3)
+    R_fit = convert_radius(Rp, hf, qmass, inc, aR, conv=conv)
+    assert np.isclose(R_fit, 0.10342083, rtol=1e-3)
 
     # Test conversion from deformed to spherical radius
-    R_pl = 1.011
-    hf = 0.1
-    qmass = 0.01
+    Rv = 0.1
+    hf = 1.5
+    qmass = 0.00098
     inc = 90.0
-    aR = 10.0
+    aR = 3.0
     conv = "Rv2Rp"
-    R_fit = convert_radius(R_pl, hf, qmass, inc, aR, conv=conv)
-    assert np.isclose(R_fit, 1.0, rtol=1e-3)
+    R_fit = convert_radius(Rv, hf, qmass, inc, aR, conv=conv)
+    assert np.isclose(R_fit, 0.096992882, rtol=1e-3)
