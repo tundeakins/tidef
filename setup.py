9a0bd2ce56b4a4377d79290dd7ef008ccc937a55
tundeakins <babatundeakinsanmi@gmail.com>
2023-11-01 14:59:53 +0100

    again

--- setup.py
+++ setup.py
@@ -19,5 +19,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=["numpy", "scipy", "matplotlib", "batman-package"],
     extras_require = { "dev": [ "pytest >=3.7 "],},
 )
